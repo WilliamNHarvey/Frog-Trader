dc26c951581185b61e6cadd7977a229d3cb04c5a
WilliamNHarvey <williamnharvey@gmail.com>
2017-07-14 22:52:49 -0400

    test

--- client/templates/tappage/tap-button/tap-button.directive.js
+++ client/templates/tappage/tap-button/tap-button.directive.js
@@ -4,39 +4,39 @@ angular.module('pepeTrader.tap', [])
     return {
         restrict: 'AE',
         scope: true,
-        template: '<div class="positioner"><div class="tapbutton" layout-align="center center" ng-click="tapButton()"></div></div>',
+        template: '<div id="tapbutton" class="tapbutton" layout-align="center center" ng-click="tapButton()"></div>',
         controller: function($scope, $element){
             $scope.tapButton = function(){
-                $('.tapbutton').animate({
+                $('#tapbutton').animate({
 
                     'width': "-=20",
                     'height': "-=20",
-                    'top': parseInt($('.tapbutton').css('top')) - 20,
-                    'left': parseInt($('.tapbutton').css('left')) - 20
+                    'top': parseInt($('#tapbutton').css('top')) - 20,
+                    'left': parseInt($('#tapbutton').css('left')) - 20
 
                 }, 200);
-                $('.tapbutton').animate({
+                $('#tapbutton').animate({
 
                     'width': "+=30",
-                    'height': "+=30"
-                    'top': parseInt($('.tapbutton').css('top')) + 30,
-                    'left': parseInt($('.tapbutton').css('left')) + 30
+                    'height': "+=30",
+                    'top': parseInt($('#tapbutton').css('top')) + 30,
+                    'left': parseInt($('#tapbutton').css('left')) + 30
 
                 }, 200);
-                $('.tapbutton').animate({
+                $('#tapbutton').animate({
 
                     'width': "-=20",
-                    'height': "-=20"
-                    'top': parseInt($('.tapbutton').css('top')) - 20,
-                    'left': parseInt($('.tapbutton').css('left')) - 20
+                    'height': "-=20",
+                    'top': parseInt($('#tapbutton').css('top')) - 20,
+                    'left': parseInt($('#tapbutton').css('left')) - 20
 
                 }, 200);
-                $('.tapbutton').animate({
+                $('#tapbutton').animate({
 
                     'width': "+=10",
-                    'height': "+=10"
-                    'top': parseInt($('.tapbutton').css('top')) + 10,
-                    'left': parseInt($('.tapbutton').css('left')) + 10
+                    'height': "+=10",
+                    'top': parseInt($('#tapbutton').css('top')) + 10,
+                    'left': parseInt($('#tapbutton').css('left')) + 10
 
                 }, 200);
             }
