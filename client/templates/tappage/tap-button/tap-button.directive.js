53f01e2ef5b88d6bb03380b943436d9247bd29ab
WilliamNHarvey <williamnharvey@gmail.com>
2017-07-14 23:26:14 -0400

    test

--- client/templates/tappage/tap-button/tap-button.directive.js
+++ client/templates/tappage/tap-button/tap-button.directive.js
@@ -6,45 +6,52 @@ angular.module('pepeTrader.tap', [])
         scope: true,
         template: '<div id="tapbutton" class="tapbutton" layout-align="center center" ng-click="tapButton()"></div>',
         controller: function($scope, $element){
+            $scope.transforming = true;
             $scope.tapButton = function(){
 
-                $('#tapbutton').stop(true).fadeTo('fast',1);
-                $('#tapbutton').css('width', '300px');
+
+                //$('#tapbutton').stop(true).fadeTo('fast',1);
+                /*$('#tapbutton').css('width', '300px');
                 $('#tapbutton').css('height', '300px');
                 $('#tapbutton').css('top', '50%');
-                $('#tapbutton').css('left', '50%');
-                $('#tapbutton').animate({
+                $('#tapbutton').css('left', '50%');*/
+                if(!$scope.transforming) {
 
-                    'width': "-=20",
-                    'height': "-=20",
-                    'top': parseInt($('#tapbutton').css('top')) + 10,
-                    'left': parseInt($('#tapbutton').css('left')) + 10
+                    $('#tapbutton').animate({
 
-                }, 150);
-                $('#tapbutton').animate({
+                        'width': "-=20",
+                        'height': "-=20",
+                        'top': parseInt($('#tapbutton').css('top')) + 10,
+                        'left': parseInt($('#tapbutton').css('left')) + 10
 
-                    'width': "+=30",
-                    'height': "+=30",
-                    'top': "-=15",
-                    'left': "-=15"
+                    }, 150);
+                    $('#tapbutton').animate({
 
-                }, 150);
-                $('#tapbutton').animate({
+                        'width': "+=30",
+                        'height': "+=30",
+                        'top': "-=15",
+                        'left': "-=15"
 
-                    'width': "-=20",
-                    'height': "-=20",
-                    'top': parseInt($('#tapbutton').css('top')) + 5,
-                    'left': parseInt($('#tapbutton').css('left')) + 5
+                    }, 150);
+                    $('#tapbutton').animate({
 
-                }, 150);
-                $('#tapbutton').animate({
+                        'width': "-=20",
+                        'height': "-=20",
+                        'top': parseInt($('#tapbutton').css('top')) + 5,
+                        'left': parseInt($('#tapbutton').css('left')) + 5
 
-                    'width': "+=10",
-                    'height': "+=10",
-                    'top': "-=5",
-                    'left': "-=5"
+                    }, 150);
+                    $('#tapbutton').animate({
 
-                }, 150);
+                        'width': "+=10",
+                        'height': "+=10",
+                        'top': "-=5",
+                        'left': "-=5"
+
+                    }, 150);
+                    $scope.transforming = false;
+                }
+
             }
         }
     }
