c6796b51018d5968db0fb066747267bafacbefe9
WilliamNHarvey <williamnharvey@gmail.com>
2017-07-14 21:41:19 -0400

    test

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -190,21 +190,3 @@ angular.module('pepeTrader.controllers', [])
 
   }
 })
-.directive('tapButton', function(){
-    return {
-        restrict: 'AE',
-        scope: true,
-        template: '<div class="tapbutton" layout-align="center center" ng-click="tapButton()"></div>',
-        controller: function($scope, $element){
-            $scope.tapbutton = function(){
-                $(this).animate({
-                    "width": "-=10",
-                    "height": "+=10"
-                }, 200, function() {
-                    "width": "+=10",
-                        "height": "+=10"
-                });
-            }
-        }
-    }
-})
