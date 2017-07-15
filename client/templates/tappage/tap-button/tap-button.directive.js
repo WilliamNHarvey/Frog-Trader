c7de0a8527263ddecea8cf14164763cb4fa2e21c
WilliamNHarvey <williamnharvey@gmail.com>
2017-07-14 23:21:36 -0400

    test

--- client/templates/tappage/tap-button/tap-button.directive.js
+++ client/templates/tappage/tap-button/tap-button.directive.js
@@ -7,7 +7,12 @@ angular.module('pepeTrader.tap', [])
         template: '<div id="tapbutton" class="tapbutton" layout-align="center center" ng-click="tapButton()"></div>',
         controller: function($scope, $element){
             $scope.tapButton = function(){
+
                 $('#tapbutton').stop(true).fadeTo('fast',1);
+                $('#tapbutton').css('width', '300px');
+                $('#tapbutton').css('height', '300px');
+                $('#tapbutton').css('top', '50%');
+                $('#tapbutton').css('left', '50%');
                 $('#tapbutton').animate({
 
                     'width': "-=20",
@@ -15,7 +20,7 @@ angular.module('pepeTrader.tap', [])
                     'top': parseInt($('#tapbutton').css('top')) + 10,
                     'left': parseInt($('#tapbutton').css('left')) + 10
 
-                }, 200);
+                }, 50);
                 $('#tapbutton').animate({
 
                     'width': "+=30",
@@ -23,7 +28,7 @@ angular.module('pepeTrader.tap', [])
                     'top': "-=15",
                     'left': "-=15"
 
-                }, 200);
+                }, 50);
                 $('#tapbutton').animate({
 
                     'width': "-=20",
@@ -31,7 +36,7 @@ angular.module('pepeTrader.tap', [])
                     'top': parseInt($('#tapbutton').css('top')) + 5,
                     'left': parseInt($('#tapbutton').css('left')) + 5
 
-                }, 200);
+                }, 50);
                 $('#tapbutton').animate({
 
                     'width': "+=10",
@@ -39,7 +44,7 @@ angular.module('pepeTrader.tap', [])
                     'top': "-=5",
                     'left': "-=5"
 
-                }, 200);
+                }, 50);
             }
         }
     }
