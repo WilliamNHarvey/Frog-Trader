05492f0d42264a7ac4e3725c9be14d3f6ad696e8
WilliamNHarvey <williamnharvey@gmail.com>
2017-07-14 21:58:54 -0400

    test

--- client/templates/tappage/tap-button.component.js
+++ client/templates/tappage/tap-button.component.js
@@ -7,13 +7,7 @@ angular.module('pepeTrader.tap', [])
         template: '<div class="tapbutton" layout-align="center center" ng-click="tapButton()"></div>',
         controller: function($scope, $element){
             $scope.tapbutton = function(){
-                $(this).animate({
-                    "width": "-=10",
-                    "height": "+=10"
-                }, 200, function() {
-                    "width": "+=10",
-                    "height": "+=10"
-                });
+                
             }
         }
     }
