e87547a05325841f7963396788df38e1e61435f3
WilliamNHarvey <williamnharvey@gmail.com>
2017-07-17 20:25:27 -0400

    bug

--- client/templates/tappage/tap-button/tap-button.directive.js
+++ client/templates/tappage/tap-button/tap-button.directive.js
@@ -6,15 +6,23 @@ angular.module('pepeTrader.tap', [])
         scope: true,
         template: '<div id="tapbutton" class="tapbutton" layout-align="center center" ng-click="tapButton($event)"></div>',
         controller: function($scope, $element){
-
-
             $scope.tapButton = function(e){
+                var newpep = $('<div class="pepimg fadeupandout"></div>')
+                    .css({
+                        "left": e.pageX + 'px',
+                        "top": e.pageY + 'px'
+
+                    })
+                    .appendTo(document.body);
+                setTimeout(function(){ newpep.remove() }, 1500);
 
                 $('#tapbutton').stop(true).fadeTo('fast',1);
-                $('#tapbutton').css('width', '300px');
-                $('#tapbutton').css('height', '300px');
-                $('#tapbutton').css('top', '50%');
-                $('#tapbutton').css('left', '50%');
+                if($('#tapbutton').width() != 300) {
+                    $('#tapbutton').css('width', '300px');
+                    $('#tapbutton').css('height', '300px');
+                    $('#tapbutton').css('top', '50%');
+                    $('#tapbutton').css('left', '50%');
+                }
                 $('#tapbutton').animate({
 
                     'width': "-=20",
@@ -47,16 +55,6 @@ angular.module('pepeTrader.tap', [])
                     'left': "-=5"
 
                 }, 150);
-
-                var newpep = $('<div class="pepimg fadeupandout"></div>')
-                    .css({
-                        "left": e.pageX + 'px',
-                        "top": e.pageY + 'px'
-
-                    })
-                    .appendTo(document.body);
-                setTimeout(function(){ newpep.remove() }, 1500);
-
             }
         }
     }
