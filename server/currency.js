2c08c89154f0ee50bc4b26a7bcd7fe5be5a85e62
WilliamNHarvey <williamnharvey@gmail.com>
2017-07-19 14:16:10 -0400

    test

---
+++ server/currency.js
@@ -0,0 +1,37 @@
+/*module.exports = function(models) {
+  function pepes(req, res, next) {
+    var user = req.body;
+
+    new models.User({
+      deviceId: user.deviceId
+    }).fetch().then(function(model) {
+      if (model) {
+          res.json(model.attributes)
+      } else {
+        encryptPassword(user.password, function(err, hash) {
+          if (err) return next(err);
+
+          user.token = uuid.v4();
+          user.crypted_password = hash;
+
+          delete user['password'];
+          delete user['password2'];
+
+          new models.User(user).save().then(function(model) {
+            if (register_callback) {
+              register_callback(model);
+            }
+            res.json(clean_user(model.attributes));
+
+          }).catch(next);
+        });
+      }
+    });
+  }
+
+
+
+  return {
+    pepes: pepes
+  }
+}*/
