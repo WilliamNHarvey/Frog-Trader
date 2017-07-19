36aa0986dd3643a535256a3fc120a94bb75e683f
WilliamNHarvey <williamnharvey@gmail.com>
2017-07-19 16:00:49 -0400

    test

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -22,7 +22,7 @@ angular.module('pepeTrader.controllers', [])
     };
     $scope.parsePepes = function(n){
         var base = floor(log(abs(n))/log(1000));
-        var suffix = ' ' + ['kilo','Mega','Giga','Tera','Peta','Exa','Zetta','Yotta'][base-1];
+        var suffix = ' ' + ['', 'kilo','Mega','Giga','Tera','Peta','Exa','Zetta','Yotta'][base];
         round(n/pow(1000,base), precision) == 1 ? suffix += 'pepe' : suffix += 'pepes';
         return suffix ? round(n/pow(1000,base), precision)+suffix : ''+n;
     };
