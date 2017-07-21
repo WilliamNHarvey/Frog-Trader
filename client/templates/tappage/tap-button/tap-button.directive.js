9a4e91de15e2505ef0e755fc647fc5adf185d53b
WilliamNHarvey <williamnharvey@gmail.com>
2017-07-20 22:31:09 -0400

    happy

--- client/templates/tappage/tap-button/tap-button.directive.js
+++ client/templates/tappage/tap-button/tap-button.directive.js
@@ -79,16 +79,16 @@ angular.module('pepeTrader.tap', [])
 
             function makeHappy() {
                 sad = false;
-                //$('#tapbutton').addClass('sadtohappy');
-                $('#tapbutton').css('margin-top', '117px');
-                $('#tapbutton').css('background-image', "url(../img/sadtohappypepe.gif?a="+Math.random()+')');
+                $('#tapbutton').addClass('sadtohappy');
+                $('#tapbutton').offsetHeight;
+                //$('#tapbutton').css('background-image', "url(../img/sadtohappypepe.gif?a="+Math.random()+')');
                 setTimeout(function(){
                     $('#tapbutton').removeClass('sad');
                 }, 200);
                 setTimeout(function(){
                     $('#tapbutton').addClass('happy');
                     setTimeout(function(){
-                        $('#tapbutton').css('background-image', "");
+                        //$('#tapbutton').css('background-image', "");
                         $('#tapbutton').removeClass('sadtohappy');
                     }, 200);
                 }, 1450);
@@ -96,16 +96,16 @@ angular.module('pepeTrader.tap', [])
 
             function makeSad() {
                 sad = true;
-                //$('#tapbutton').addClass('happytosad');
-                $('#tapbutton').css('margin-top', '117px');
-                $('#tapbutton').css('background-image', "url(../img/happytosadpepe.gif?x="+Math.random()+')');
+                $('#tapbutton').addClass('happytosad');
+                $('#tapbutton').offsetHeight;
+                //$('#tapbutton').css('background-image', "url(../img/happytosadpepe.gif?x="+Math.random()+')');
                 setTimeout(function(){
                     $('#tapbutton').removeClass('happy');
                 }, 200);
                 setTimeout(function(){
                     $('#tapbutton').addClass('sad');
                     setTimeout(function(){
-                        $('#tapbutton').css('background-image', "");
+                        //$('#tapbutton').css('background-image', "");
                         $('#tapbutton').removeClass('happytosad');
                     }, 200);
                 }, 1450);
