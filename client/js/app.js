f86fe671a0cce37bca116547fcde643226f0faa0
WilliamNHarvey <williamnharvey@gmail.com>
2017-07-22 20:05:59 -0400

    transitions

--- client/js/app.js
+++ client/js/app.js
@@ -60,6 +60,25 @@ angular.module('pepeTrader', ['ionic', 'pepeTrader.controllers', 'pepeTrader.ser
         'happy',
         'sad'
     ]);
+
+    $rootScope.transition = function (path, pageAnimationClass) {
+
+        if (typeof(pageAnimationClass) === 'undefined') { // Use a default, your choice
+            $scope.pageAnimationClass = 'crossFade';
+        }
+
+        else { // Use the specified animation
+            $scope.pageAnimationClass = pageAnimationClass;
+        }
+
+        if (path === 'back') { // Allow a 'back' keyword to go to previous page
+            $window.history.back();
+        }
+
+        else { // Go to the specified path
+            $location.path(path);;
+        }
+    };
 })
 
 .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
