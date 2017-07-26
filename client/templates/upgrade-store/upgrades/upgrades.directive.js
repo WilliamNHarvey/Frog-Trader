cd307d3255ae968ccb3d729d65e4185e06d48524
WilliamNHarvey <williamnharvey@gmail.com>
2017-07-26 18:08:40 -0400

    test

--- client/templates/upgrade-store/upgrades/upgrades.directive.js
+++ client/templates/upgrade-store/upgrades/upgrades.directive.js
@@ -6,7 +6,42 @@ angular.module('pepeTrader.upgrades', [])
         scope: {
             upgrade: '='
         },
-        template: '<table class="table table-striped"><tbody><tr><td class="col-md-9">description</td><td class="col-md-3">button</td></tr><tr><td class="col-md-9">description</td><td class="col-md-3">button</td></tr><tr><td class="col-md-9">description</td><td class="col-md-3">button</td></tr></tbody></table>',
+        template:   '<table class="table table-striped">' +
+                        '<tbody>' +
+                            '<tr>' +
+                                '<td class="col-md-9">description</td>' +
+                                '<td class="col-md-3">button</td>' +
+                            '</tr>' +
+                            '<tr>' +
+                                '<td class="col-md-9">description</td>' +
+                                '<td class="col-md-3">button</td>' +
+                            '</tr>' +
+                            '<tr>' +
+                                '<td class="col-md-9">description</td>' +
+                                '<td class="col-md-3">button</td>' +
+                            '</tr>' +
+                            '<tr>' +
+                                '<td class="col-md-9">description</td>' +
+                                '<td class="col-md-3">button</td>' +
+                            '</tr>' +
+                            '<tr>' +
+                                '<td class="col-md-9">description</td>' +
+                                '<td class="col-md-3">button</td>' +
+                            '</tr>' +
+                            '<tr>' +
+                                '<td class="col-md-9">description</td>' +
+                                '<td class="col-md-3">button</td>' +
+                            '</tr>' +
+                            '<tr>' +
+                                '<td class="col-md-9">description</td>' +
+                                '<td class="col-md-3">button</td>' +
+                            '</tr>' +
+                            '<tr>' +
+                                '<td class="col-md-9">description</td>' +
+                                '<td class="col-md-3">button</td>' +
+                            '</tr>' +
+                        '</tbody>' +
+                    '</table>',
         controller: function($scope){
 
         }
