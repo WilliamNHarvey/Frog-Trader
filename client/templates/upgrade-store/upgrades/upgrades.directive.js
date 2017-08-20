3a31eb491fecd410bbad70cf115bae91c2841816
WilliamNHarvey <williamnharvey@gmail.com>
2017-08-19 22:17:03 -0400

    shop directive

--- client/templates/upgrade-store/upgrades/upgrades.directive.js
+++ client/templates/upgrade-store/upgrades/upgrades.directive.js
@@ -6,8 +6,8 @@ angular.module('pepeTrader.upgrades', [])
         scope: {
             upgrade: '@'
         },
-        template:   '<td class="col-md-9">description</td>' +
-                    '<td class="col-md-3">button</td>',
+        template:   '<td class="col-md-9">{{description}}</td>' +
+                    '<td class="col-md-3">{{button}}</td>',
         controller: function(scope){
             scope.description = scope.upgrade[0];
             scope.button = scope.upgrade[2];
